82e91e08cbd3779cb8caeb739ccbace581b5b764
Vinicius Cardoso <vinny.cardoso96@gmail.com>
2020-03-26 15:18:36 -0300

    backend application

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+/**
+ * Rota / Recurso
+ */
+
+ /**
+  * Metodos HTTP:
+  * GET: Buscar uma informacao do backend
+  * POST: Criar uma informacao no backend
+  * PUT: ALterar uma informacao no backend
+  * DELETE: Deletar uma informacao no backend
+  */
+
+  /**
+   * Tipos de parametros:
+   * Query Params: Parametros nomeados enviados na rota apos "?" (filtros, paginacao)
+   * Route Params: Parametros utilizados para identificar recursos
+   * Request Body: Corpo da requisicao, utilizado para criar ou alterar recursos
+   */
+
+
+app.listen(3333);
